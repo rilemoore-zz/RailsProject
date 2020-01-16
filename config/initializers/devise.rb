8cb1d73d6c318ecfce1bbfb67a5dac676bbbbb17
rilemoore <rilemoore@gmail.com>
2020-01-15 17:26:00 -0800

    github connection established

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,9 +259,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-    ENV['GITHUB_KEY']
-    ENV['GITHUB_SECRET']
+   config.omniauth :github, '38a38f3882fc1d7d0441', '553da752c264e72d480c46ace8ea5b69ef632d3f', scope: 'user,public_repo'
+  
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, :controllers => {registrations: 'registrations'}
     devise_scope :user do
       get 'login', to: 'devise/sessions#new'
     end
     
     devise_scope :user do
-      get 'signup', to: 'devise/registations#new'
+      get 'signup', to: 'devise/registrations#new'
     end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
